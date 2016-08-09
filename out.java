d3dcbd496657bca1bc21cb04e4d4d049c01e8b13
ajithvijay001 <ajith007.v@gmail.com>
2016-08-09 18:54:07 +0530

    Update out.java

--- out.java
+++ out.java
@@ -7,6 +7,10 @@ public static void main(String s[])
 {
   Scanner sc=new Scanner(System.in);
   int n=sc.nextInt();
+  if(n<0)
+  {
+    System.out.println("enter +ve value");
+  }
   if(n%2==0)
   {
     System.out.println("even");
