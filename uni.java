f5b39a3a6beb1a5512d1373beda27f313d510819
ajithvijay001 <ajith007.v@gmail.com>
2016-08-13 13:43:35 +0530

    Create uni.java

---
+++ uni.java
@@ -0,0 +1,32 @@
+import java.io.*;
+import java.util.*;
+import java.lang.*;
+class uni
+{
+ public static void main(String []args)
+ {
+  Scanner sc=new Scanner(System.in);
+  int n=sc.nextInt();
+  int i;
+  if(n%2!=0)
+  {
+   int arr[]=new int[n];
+   for(i=0;i<n;i++)
+   {
+     arr[i]=sc.nextInt();
+    }
+    Arrays.sort(arr);
+    for(i=0;i<arr.length;i++)
+    {
+    if(arr[i]==arr[i+1])
+    {
+    }
+    else
+    {
+     break;
+    }
+    }
+  }
+ }
+}
+
