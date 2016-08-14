38c46139399f8f29fa2fff2eee465bf16892deba
ajithvijay001 <ajith007.v@gmail.com>
2016-08-14 12:35:32 +0530

    Create RevDigi.java

---
+++ RevDigi.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+class RevDigi
+{
+ public static void main(String args[])
+ {
+  Scanner sc=new Scanner(System.in);
+  int n=sc.nextInt();
+  int temp=0;t=0;
+  while(n>0)
+  {
+   temp=n%10;
+   t=10*t+temp;
+   n=n/10;
+  }
+  System.out.println(t);
+ }
+}
