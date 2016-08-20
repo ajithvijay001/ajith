6ad44929633ea3500222e920f66cf4cb3ab5152b
ajithvijay001 <ajith007.v@gmail.com>
2016-08-20 10:35:12 +0530

    Create target_num.java

---
+++ target_num.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+public class rev
+{
+	public static void main(String[] args) 
+	{
+		Scanner sc=new Scanner(System.in);
+	
+		int arr[]=new int[sc.nextInt()];
+		int t=sc.nextInt();
+		for(int i=0;i<arr.length;i++)
+			arr[i]=sc.nextInt();
+		System.out.println(check_trgt(arr,t));
+	}
+
+	private static String check_trgt(int[] arr, int t)
+	{
+		String str="";
+		for(int i=0;i<arr.length;i++)
+		{
+			for(int j=i+1;j<arr.length;j++)
+			{
+				if(arr[i]+arr[j]==t)
+					str+=arr[i]+","+arr[j]+"  ";
+			}
+		}
+		
+		return str;
+	}
+
+}
