55e4dab418c032eedb8ad063b01dc0c9e8fc35ea
ajithvijay001 <ajith007.v@gmail.com>
2016-08-24 18:52:23 +0530

    Update k_digit.java

--- k_digit.java
+++ k_digit.java
@@ -4,16 +4,18 @@ class k_digit
 {
  public static void main(String []args)
  {
-  Scanner sc=new Scanner(System.in);
-  String s=sc.next();
-  int n=sc.nextInt();
-  ArrayList<Interger> al=new ArrayList<Integer>();
-  char ch[]=s.toCharArray();
-  for(char i:ch)
-    al.add(Integer.parseInt(i+"");
-  Collections.sort(al);
-  for(int j=0;j<n;j++)
-     al.remove(al.size()-1);
-  System.out.println(al);
+  String s=new Scanner(System.in).next();
+	String str="";
+	int n=new Scanner(System.in).nextInt();
+	ArrayList al=new ArrayList();
+	char ch[]=s.toCharArray();
+	for(char i:ch)
+		al.add(Integer.parseInt(i+""));
+	Collections.sort(al);
+	for(int j=0;j<n;j++)
+		al.remove(al.size()-1);
+	for(int j=0;j<al.size();j++)
+	   str+=al.get(j);
+	System.out.println(str);
  }
 }
